cde18f4050ef9d29dcd97b3422594bd05a62f8da
Foleas <sebastianfilipetto@gmail.com>
2023-08-22 13:28:38 -0300

    Tailwind custom colors

---
+++ src/tailwind/colors.js
@@ -0,0 +1,16 @@
+export const tailwindCustomColors = {
+  blue: {
+    moderate: "#5457b6",
+    lightGrayish: "#c3c4ef",
+    grayish: "#67727e",
+    dark: "#324152",
+  },
+  red: {
+    soft: "#ed6468",
+    pale: "#ffb8bb",
+  },
+  gray: {
+    veryLight: "#f5f6fa",
+    light: "#eaecf1",
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+
+import { tailwindCustomColors } from "./src/tailwind/colors";
+
 export default {
   darkMode: "class",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: tailwindCustomColors.blue,
+        red: tailwindCustomColors.red,
+        gray: tailwindCustomColors.gray,
+      },
+    },
   },
   plugins: [],
 };
