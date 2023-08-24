d46c559926f212e1911deefc604840727a984739
Foleas <sebastianfilipetto@gmail.com>
2023-08-24 12:50:04 -0300

    Comments date to timestamp

---
+++ src/utils/getDate.ts
@@ -0,0 +1,28 @@
+export const getFormatedDate = (timestamp: number) => {
+  const now = Date.now();
+  const diffMilliseconds = now - timestamp;
+
+  const seconds = Math.floor(diffMilliseconds / 1000);
+  const minutes = Math.floor(seconds / 60);
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
+  const months = Math.floor(days / 30); // Approximation
+  const years = Math.floor(days / 365); // Approximation
+
+  if (seconds < 60) {
+    return `${seconds} sec ago`;
+  } else if (minutes < 60) {
+    return `${minutes} min ago`;
+  } else if (hours < 24) {
+    return `${hours} hr ago`;
+  } else if (days < 7) {
+    return `${days} day${days !== 1 ? "s" : ""} ago`;
+  } else if (weeks < 4) {
+    return `${weeks} week${weeks !== 1 ? "s" : ""} ago`;
+  } else if (months < 12) {
+    return `${months} month${months !== 1 ? "s" : ""} ago`;
+  } else {
+    return `${years} year${years !== 1 ? "s" : ""} ago`;
+  }
+};
