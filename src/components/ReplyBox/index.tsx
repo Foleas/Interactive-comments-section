87022b5a8bec7d440ea2d9c2fb993000d8ea7c3d
Foleas <sebastianfilipetto@gmail.com>
2023-09-26 18:34:41 -0300

    Reply Box Mobile

--- src/components/ReplyBox/index.tsx
+++ src/components/ReplyBox/index.tsx
@@ -22,18 +22,23 @@ const ReplyBox = ({
   const [comment, setComment] = useState(replyingTo ? `@${replyingTo} ` : "");
 
   return (
-    <div className="comment bg-white shadow-md rounded-md p-5 flex gap-5 items-start mb-5">
-      <img className="w-[40px]" src={user?.image.png} alt={user?.username} />
+    <div className="comment bg-white shadow-md rounded-md p-5 flex flex-wrap gap-5 items-start mb-5 max-sm:justify-between">
+      <img
+        className="w-[40px] max-sm:order-1"
+        src={user?.image.png}
+        alt={user?.username}
+      />
       <textarea
         name="comment"
         placeholder="Add a comment..."
-        className="rounded-md transition-colors border border-gray-light focus:border-blue-moderate outline-none h-[96px] py-3 px-6 resize-none flex-1"
+        className="rounded-md transition-colors border border-gray-light focus:border-blue-moderate outline-none h-[96px] py-3 px-6 resize-none flex-1 max-sm:flex-[0_0_100%] max-sm:order-0"
         value={comment}
         onChange={({ target }) => setComment(target.value)}
       />
       <Button
         text={buttonText}
         disabled={comment === ""}
+        customClass="max-sm:order-2"
         onClickHandler={() => {
           addComment(user, comment, replyingTo, parentId);
           setComment("");
--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -2,16 +2,19 @@ interface ButtonProps {
   text: string;
   disabled: boolean;
   onClickHandler: () => void;
+  customClass?: string;
 }
 
 const buttonClass =
   "transition-colors bg-blue-moderate hover:bg-blue-lightGrayish text-white font-medium py-3 px-5 rounded-md";
 
 const Button = (props: ButtonProps) => {
-  const { text, disabled, onClickHandler } = props;
+  const { text, disabled, onClickHandler, customClass = "" } = props;
   return (
     <button
-      className={`${buttonClass} ${disabled ? "cursor-not-allowed" : ""}`}
+      className={`${buttonClass} ${
+        disabled ? "cursor-not-allowed" : ""
+      } ${customClass}`}
       disabled={disabled}
       onClick={() => onClickHandler()}
     >
