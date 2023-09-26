c18e33ff2a85398dbaaaef1d749e05c47c74b01a
Foleas <sebastianfilipetto@gmail.com>
2023-09-26 12:37:27 -0300

    comment Service update

--- src/services/commentService.ts
+++ src/services/commentService.ts
@@ -1,5 +1,4 @@
 import httpService from "./httpService";
-// import storageService from "../utils/localStorage";
 import { CommentItem, CommentUser } from "../types";
 
 const getAll = async (url: string): Promise<CommentItem[]> => {
@@ -11,26 +10,5 @@ const getCurrentUser = async (url: string): Promise<CommentUser> => {
   const allData = await httpService.fetchData(url);
   return allData.currentUser;
 };
-/*
-export const deleteItem = (id:number; comments:CommentItem[]) => {
-  for (let i = 0; i < comments.length; i++) {
-    const comment = comments[i];
-
-    if (comment.id === id) {
-      // Remove the item at index i
-      comments.splice(i, 1);
-      return true; // Item was found and deleted
-    }
-
-    if (comment.replies && comment.replies.length > 0) {
-      // Recursively search for the ID in the replies
-      if (deleteItem(id, comment.replies)) {
-        return true; // Reply was found and deleted
-      }
-    }
-  }
-  return false;
-};
-*/
 
 export default { getAll, getCurrentUser };
